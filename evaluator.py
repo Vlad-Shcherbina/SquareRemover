9dc4a947b03903380f1b62de0c6fa07f81838758
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2014-04-12 18:41:27 +0200

    compute stats

--- evaluator.py
+++ evaluator.py
@@ -2,10 +2,26 @@ import sys
 import multiprocessing
 import runner
 import pickle
+from math import sqrt
+import collections
 
 
 def worker(task):
     return runner.run_solution(*task)
+
+
+def list_stats(xs):
+    xs = list(xs)
+    if not xs:
+        return 'empty'
+    if len(xs) == 1:
+        return str(xs[0])
+    sx = sum(xs)
+    sx2 = sum(x * x for x in xs)
+    ave = 1.0 * sx / len(xs)
+    sigma = sqrt((sx2 - 2 * sx * ave + len(xs) * ave * ave) / (len(xs) - 1))
+    return '{:.3f} &plusmn; <i>{:.3f}</i>'.format(ave, sigma)
+
 
 def main():
     assert len(sys.argv) == 2
@@ -16,9 +32,9 @@ def main():
 
     types = [(n, colors) for colors in (4, 5, 6) for n in range(8, 16 + 1)]
 
-    tasks = [(command, by_problem_type[t][0]) for t in types]
+    tasks = [(command, by_problem_type[t][i]) for t in types for i in range(2)]
 
-    map = multiprocessing.Pool(6).imap
+    map = multiprocessing.Pool(5).imap
 
     results = []
     for result in map(worker, tasks):
@@ -31,8 +47,39 @@ def main():
     with open('results.pickle', 'rb') as fin:
         results = pickle.load(fin)
 
-    sum_score = sum(result.score for result in results)
-    print('average score: {:.3f}'.format(sum_score / len(results)))
+    with open('stats.html', 'w') as fout:
+        fout.write('<table>')
+        fout.write('<tr> <th></th>')
+        for colors in None, 4, 5, 6:
+            fout.write('<th>colors = {}</th>'.format(colors or '*'))
+        fout.write('</tr>')
+        for n in [None] + list(range(8, 16 + 1)):
+            fout.write('<tr>')
+            fout.write('<th>n = {}</th>'.format(n or '*'))
+            for colors in None, 4, 5, 6:
+                fout.write('<td align="right">')
+
+                scores = []
+                values = collections.defaultdict(list)
+                for result in results:
+                    if n and n != result.n:
+                        continue
+                    if colors and colors != result.colors:
+                        continue
+                    for dp in result.data_points:
+                        for k, v in dp.items():
+                            if k != 'type':
+                                values[k].append(v)
+                    scores.append(result.score)
+                fout.write('<b>score = {}</b>'.format(list_stats(scores)))
+                for k, xs in values.items():
+                    fout.write('<br>{} = {}'.format(k, list_stats(xs)))
+
+                fout.write('</td>')
+            fout.write('</tr>')
+        fout.write('</table>')
+
+    print('score:', list_stats(result.score for result in results))
 
 
 if __name__ == '__main__':
