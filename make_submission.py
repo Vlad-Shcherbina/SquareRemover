e5cd1f29fd93f56143ed2bea78b4083170c102f4
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2014-04-13 16:16:54 +0200

    delete old python solution and related stuff

--- make_submission.py
+++
@@ -1,5 +0,0 @@
-with open('_submission.py', 'w') as fout:
-    fout.write(
-        open('sol.py').read().replace(
-            'from patterns import *',
-            open('patterns.py').read().replace('__main__', 'zzz')))
